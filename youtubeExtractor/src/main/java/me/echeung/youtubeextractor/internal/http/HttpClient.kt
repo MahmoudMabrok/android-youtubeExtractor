c8d5f73945e8830b888ed4033c7147b5ea5d0934
arkon <eugcheung94@gmail.com>
2021-01-30 19:16:09 -0500

    Update user agent string

--- youtubeExtractor/src/main/java/me/echeung/youtubeextractor/internal/http/HttpClient.kt
+++ youtubeExtractor/src/main/java/me/echeung/youtubeextractor/internal/http/HttpClient.kt
@@ -28,6 +28,6 @@ class HttpClient {
 
     companion object {
         private const val USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36 Edg/88.0.705.50"
     }
 }
